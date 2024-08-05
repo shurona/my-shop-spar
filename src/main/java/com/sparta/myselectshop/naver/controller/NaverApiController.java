9721a2c8be67da22dabfffb90c7a0a1769108c95
shurona <khc4669@naver.com>
2024-08-05 23:01:27 +0900

    naver api 연동

---
+++ src/main/java/com/sparta/myselectshop/naver/controller/NaverApiController.java
@@ -0,0 +1,23 @@
+package com.sparta.myselectshop.naver.controller;
+
+
+import com.sparta.myselectshop.naver.dto.ItemDto;
+import com.sparta.myselectshop.naver.service.NaverApiService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class NaverApiController {
+
+    private final NaverApiService naverApiService;
+
+    @GetMapping("/search")
+    public List<ItemDto> searchItems(
+            @RequestParam String query)  {
+        return naverApiService.searchItems(query);
+    }
+}
