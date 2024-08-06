de311fe900071f75e5c4e640c14e6587d9f63676
shurona <khc4669@naver.com>
2024-08-06 14:16:24 +0900

    폴더 기능 추가

--- src/main/java/com/sparta/myselectshop/repository/FolderRepository.java
+++ src/main/java/com/sparta/myselectshop/repository/FolderRepository.java
@@ -1,8 +1,16 @@
 package com.sparta.myselectshop.repository;
 
 import com.sparta.myselectshop.entity.Folder;
+import com.sparta.myselectshop.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 public interface FolderRepository extends JpaRepository<Folder, Long> {
+    //query = select * from folder where user_id = ? and name in (?, ? ,?);
+    List<Folder> findAllByUserAndNameIn(User user, List<String> folderNames);
+
+    List<Folder> findAllByUser(User user);
+
 }
