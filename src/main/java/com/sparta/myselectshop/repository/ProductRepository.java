d968035702ab344380653cc466ca5a1d923438d6
shurona <khc4669@naver.com>
2024-08-06 20:30:51 +0900

    쿼리 확인

--- src/main/java/com/sparta/myselectshop/repository/ProductRepository.java
+++ src/main/java/com/sparta/myselectshop/repository/ProductRepository.java
@@ -14,6 +14,10 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     Page<Product> findAllByUser(User user, PageRequest pageable);
 
     // query =>
+    // select * from product p left join product_folder pf on p.id = pf.product_id
+    // where p.user_id = ? and pf.folder_id = ?
+    // order by p.id
+    // limit 10, 10;
     Page<Product> findAllByUserAndProductFolders_FolderId(User user, Long folderId, PageRequest pageable);
 
 }
